08d9036aa5ec07b42ff32d38c178eaca26df6f03
Luis Velandia <luiseduardo1218@gmail.com>
2023-05-23 18:22:42 -0500

    09: 118. Exponer métodos del Hook

--- src/01-useState/CounterWithCustomHook.tsx
+++ src/01-useState/CounterWithCustomHook.tsx
@@ -2,15 +2,24 @@ import { useCounter } from "../hooks/useCounter";
 
 export const CounterWithCustomHook = () => {
 
-    const {counter} = useCounter();
+    const {counter, increment, decrement, reset} = useCounter();
 
   return (
     <>
       <h1>Counter with Hook: {counter}</h1>
 
-      <button className="btn btn-primary">+1</button>
-      <button className="btn btn-primary">Reset</button>
-      <button className="btn btn-primary">-1</button>
+      <button 
+        className="btn btn-primary"
+        onClick={()=> increment(2)}
+        >+1</button>
+      <button 
+        className="btn btn-primary"
+        onClick={reset}
+        >Reset</button>
+      <button 
+        className="btn btn-primary"
+        onClick={()=> decrement()}
+        >-1</button>
     </>
   );
 };
