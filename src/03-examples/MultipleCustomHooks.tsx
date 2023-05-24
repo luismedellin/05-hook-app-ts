a4073af75d37f659c1f07f995e2552da85dc2ded
Luis Velandia <luiseduardo1218@gmail.com>
2023-05-23 22:20:30 -0500

    09: 127. Conectando ambos Hooks

--- src/03-examples/MultipleCustomHooks.tsx
+++ src/03-examples/MultipleCustomHooks.tsx
@@ -1,35 +1,38 @@
+import { useState } from "react";
 import { useFetch } from "../hooks/useFetch";
+import { useCounter } from "../hooks/useCounter";
 
 export const MultipleCustomHooks = () => {
+
+    const {counter, increment} = useCounter(1);
+
   const { data, isLoading, hasError } = useFetch(
-    "https://breakingbadapi.com/api/quotes/1"
+    `https://breakingbadapi.com/api/quotes/${counter}`
   );
 
   const { author, quote } = !!data && data[0];
-  
+
+
   return (
     <>
       <h1>BreakingBad Quotes</h1>
       <hr />
 
-        {
-            isLoading ? (
-                <div className="alert alert-info text-center">
-                    Loading...
-                </div>
-            ) : (
-                <blockquote className="blockquote text-end">
-                    <p className="mb-1">{author}</p>
-                    <footer className="blockquote-footer">{quote}</footer>
-                </blockquote>
-            )
-        }
+      {isLoading ? (
+        <div className="alert alert-info text-center">Loading...</div>
+      ) : (
+        <blockquote className="blockquote text-end">
+          <p className="mb-1">{author}</p>
+          <footer className="blockquote-footer">{quote}</footer>
+        </blockquote>
+      )}
 
-        <button className="btn btn-primary">
-            Next quote
-        </button>
-
-        
+      <button 
+        className="btn btn-primary" 
+        disabled={isLoading}
+        onClick={()=> increment()}>
+        Next quote
+      </button>
     </>
   );
 };
