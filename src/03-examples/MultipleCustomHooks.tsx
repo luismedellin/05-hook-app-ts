6c61e19d6e10af3d12cffe99294f29ded7c7ed1c
Luis Velandia <luiseduardo1218@gmail.com>
2023-05-23 22:06:36 -0500

    09: 126. useFetch + useCounter

--- src/03-examples/MultipleCustomHooks.tsx
+++ src/03-examples/MultipleCustomHooks.tsx
@@ -5,12 +5,31 @@ export const MultipleCustomHooks = () => {
     "https://breakingbadapi.com/api/quotes/1"
   );
 
-  console.log(data, isLoading, hasError)
-
+  const { author, quote } = !!data && data[0];
+  
   return (
     <>
       <h1>BreakingBad Quotes</h1>
       <hr />
+
+        {
+            isLoading ? (
+                <div className="alert alert-info text-center">
+                    Loading...
+                </div>
+            ) : (
+                <blockquote className="blockquote text-end">
+                    <p className="mb-1">{author}</p>
+                    <footer className="blockquote-footer">{quote}</footer>
+                </blockquote>
+            )
+        }
+
+        <button className="btn btn-primary">
+            Next quote
+        </button>
+
+        
     </>
   );
 };
