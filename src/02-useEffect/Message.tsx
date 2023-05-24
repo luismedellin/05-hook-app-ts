685825a2ecf03e0ca9c101bb04d2941280b977ff
Luis Velandia <luiseduardo1218@gmail.com>
2023-05-23 21:15:21 -0500

    09: 122. useEffect - Precauciones

--- src/02-useEffect/Message.tsx
+++ src/02-useEffect/Message.tsx
@@ -1,20 +1,25 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 export const Message = () => {
 
+    const [coords, setCoords] = useState({x: 0, y:0})
 
-    useEffect(() => {
-      console.log('Message Mounted');
-    
-      return () => {
-        console.log('Message Unmounted');
-      }
-    }, [])
-    
+  useEffect(() => {
+    const onMouseMove = ({ x, y }) => {
+      setCoords({ x, y })
+    };
+
+    window.addEventListener("mousemove", onMouseMove);
+
+    return () => {
+        window.removeEventListener("mousemove", onMouseMove);
+    };
+  }, []);
 
   return (
     <>
-        <h3>Usuario ya existe</h3>
+      <h3>Usuario ya existe</h3>
+      <p>{JSON.stringify(coords)}</p>
     </>
-  )
-}
+  );
+};
